2cff4998a841a30810e45d5219019d63f56791a1
sato yoshiyuki <yoshi.389111@gmail.com>
2026-06-04 19:35:23 +0900

    :recycle: refact: streamline diagnostic handling and add DIAGNOSTIC_LOOKUP for better clarity

--- src/RobotsTxtDiagnosticUpdater.ts
+++ src/RobotsTxtDiagnosticUpdater.ts
@@ -1,124 +1,32 @@
 import * as vscode from "vscode";
 import * as path from "path";
 import { parseRobotsTxt, AstDirective } from "./parser/documentParser";
-import { Span } from "./parser/span";
-import { DIRECTIVE_INFOS, ParameterInfo } from "./data/directiveInfo";
+import { Span, isEmptySpan } from "./parser/span";
 import { getLogger } from "./utils/logger";
+import { DIRECTIVE_INFOS, ParameterInfo } from "./data/directiveInfo";
+import { DIAGNOSTIC_LOOKUP, DiagnosticInfo } from "./data/diagnostics";
 import * as constants from "./data/constants";
 
-interface DiagnosticInfo {
-  code: string;
-  severity: vscode.DiagnosticSeverity;
-  tag?: vscode.DiagnosticTag;
-  message: string;
-}
+/** Regular expression to validate directive names. */
+const REGEX_DIRECTIVE_NAME = /^[a-zA-Z]([a-zA-Z0-9_-]*[a-zA-Z])?$/;
 
-const DIAGNOSTIC_CODES = {
-  FILENAME_INVALID: {
-    code: "RBT001",
-    severity: vscode.DiagnosticSeverity.Error,
-    message: "Invalid file name. Expected 'robots.txt'.",
-  },
-  ENCODING_NOT_UTF8: {
-    code: "RBT002",
-    severity: vscode.DiagnosticSeverity.Error,
-    message: "Invalid file encoding. Expected UTF-8.",
-  },
-  ENCODING_UTF8_BOM: {
-    code: "RBT003",
-    severity: vscode.DiagnosticSeverity.Warning,
-    message: "The file contains a UTF-8 BOM.",
-  },
-  FILESIZE_LARGE: {
-    code: "RBT004",
-    severity: vscode.DiagnosticSeverity.Warning,
-    message: "Exceeds the recommended 500 KiB limit.",
-  },
+/** Regular expression to validate product tokens. */
+const REGEX_PRODUCT_TOKEN = /^([a-zA-Z_-]+$|^\*)$/;
 
-  DIRECTIVE_MISSING_COLON: {
-    code: "RBT101",
-    severity: vscode.DiagnosticSeverity.Error,
-    message: "Missing a colon.",
-  },
-  DIRECTIVE_NAME_INVALID: {
-    code: "RBT102",
-    severity: vscode.DiagnosticSeverity.Error,
-    message: "Invalid directive name.",
-  },
-  DIRECTIVE_UNKNOWN: {
-    code: "RBT103",
-    severity: vscode.DiagnosticSeverity.Information,
-    message: "Unknown directive.",
-  },
-  DIRECTIVE_OUTSIDE: {
-    code: "RBT104",
-    severity: vscode.DiagnosticSeverity.Warning,
-    tag: vscode.DiagnosticTag.Unnecessary,
-    message: "The directive is defined outside of a group.",
-  },
-  PRODUCT_TOKEN_INVALID: {
-    code: "RBT105",
-    severity: vscode.DiagnosticSeverity.Warning,
-    message: "Invalid product token. Allowed: [A-Za-z_-] or single '*'",
-  },
-  GROUP_MISSING_ALLOW_DISALLOW: {
-    code: "RBT106",
-    severity: vscode.DiagnosticSeverity.Error,
-    message: "No rules in the group.",
-  },
-  PATH_PATTERN_NOT_START_SLASH: {
-    code: "RBT107",
-    severity: vscode.DiagnosticSeverity.Warning,
-    message: "Does not start with a slash.",
-  },
-  PATH_PATTERN_UNNECESSARY_WILDCARD: {
-    code: "RBT108",
-    severity: vscode.DiagnosticSeverity.Hint,
-    message: "Unnecessary '*' at the end of the pattern.",
-  },
-  PATH_PATTERN_DOUBLE_ASTERISK: {
-    code: "RBT109",
-    severity: vscode.DiagnosticSeverity.Hint,
-    message: "Unnecessary '**' (use a single '*').",
-  },
-  PATH_PATTERN_DOLLAR: {
-    code: "RBT110",
-    severity: vscode.DiagnosticSeverity.Error,
-    message: "'$' is only allowed at the end of the pattern.",
-  },
-  PATH_PATTERN_INVALID_URLENCODE: {
-    code: "RBT111",
-    severity: vscode.DiagnosticSeverity.Error,
-    message: "Invalid URL characters.",
-  },
+/** Regular expression to validate encoded path patterns. */
+const REGEX_ENCODED_PATH = /^[-$%&*./0-9=?A-Z_a-z~]+$/;
 
-  URL_INVALID: {
-    code: "RBT501",
-    severity: vscode.DiagnosticSeverity.Error,
-    message: "Invalid URL.",
-  },
-  DIRECTIVE_DEPRECATED: {
-    code: "RBT502",
-    severity: vscode.DiagnosticSeverity.Information,
-    tag: vscode.DiagnosticTag.Deprecated,
-    message: "Deprecated directive.",
-  },
-  NOT_NUMERIC: {
-    code: "RBT503",
-    severity: vscode.DiagnosticSeverity.Warning,
-    message: "Value is not numeric.",
-  },
-  NUMBER_LEADING_ZERO: {
-    code: "RBT504",
-    severity: vscode.DiagnosticSeverity.Hint,
-    message: "Consider removing leading zeros.",
-  },
-  PARAM_INVALID: {
-    code: "RBT505",
-    severity: vscode.DiagnosticSeverity.Warning,
-    message: "Invalid directive value.",
-  },
-};
+/** Regular expression to validate query parameters. (e.g. "param1&param2") */
+const REGEX_QUERY_PARAMS = /^\w+(\&\w+)*$/;
+
+/** Regular expression to validate numeric values. */
+const REGEX_NUMERIC = /^\d+$/;
+
+/** Regular expression to validate numeric values with leading zeros. */
+const REGEX_LEADING_ZEROS = /^0+\d+$/;
+
+/** Recommended maximum file size. (500 KiB) */
+const FILE_SIZE_LIMIT = 500 * 1024;
 
 export class RobotsTxtDiagnosticUpdater {
   private readonly log = getLogger();
@@ -139,49 +47,17 @@ export class RobotsTxtDiagnosticUpdater {
     try {
       this.diagnostics = [];
 
-      if (!this.isValidFileName(document)) {
-        // The file name is not 'robots.txt'
-        this.addDiagnostic(
-          DIAGNOSTIC_CODES.FILENAME_INVALID,
-          new vscode.Range(0, 0, 0, 0),
-        );
-      }
-
-      if (document.encoding === "utf8bom") {
-        // The file contains a UTF-8 BOM
-        this.addDiagnostic(
-          DIAGNOSTIC_CODES.ENCODING_UTF8_BOM,
-          new vscode.Range(0, 0, 0, 0),
-        );
-      } else if (document.encoding !== "utf8") {
-        // The file encoding is not UTF-8
-        this.addDiagnostic(
-          DIAGNOSTIC_CODES.ENCODING_NOT_UTF8,
-          new vscode.Range(0, 0, 0, 0),
-        );
-      }
-
-      // Check for file size
-      const isFileSizeValid = await this.isValidFileSize(document);
-      if (!isFileSizeValid) {
-        this.addDiagnostic(
-          DIAGNOSTIC_CODES.FILESIZE_LARGE,
-          new vscode.Range(0, 0, 0, 0),
-        );
-      }
+      // Check the file itself for issues
+      await this.checkFile(document);
 
       // Parse the robots.txt file
       const astRoot = parseRobotsTxt(document);
 
       for (const astDirective of astRoot.outside.rules) {
         this.addDiagnostic(
-          DIAGNOSTIC_CODES.DIRECTIVE_OUTSIDE,
+          DIAGNOSTIC_LOOKUP.DIRECTIVE_OUTSIDE,
           astDirective.name.range,
         );
-        this.checkDirective(astDirective);
-      }
-
-      for (const astDirective of astRoot.globals) {
         this.checkDirective(astDirective);
       }
 
@@ -200,10 +76,14 @@ export class RobotsTxtDiagnosticUpdater {
         if (!hasAllowOrDisallowRule) {
           // The group does not contain allow/disallow directives
           this.addDiagnostic(
-            DIAGNOSTIC_CODES.GROUP_MISSING_ALLOW_DISALLOW,
+            DIAGNOSTIC_LOOKUP.GROUP_MISSING_ALLOW_DISALLOW,
             group.userAgents[0]!.name.range,
           );
         }
+      }
+
+      for (const astDirective of astRoot.globals) {
+        this.checkDirective(astDirective);
       }
 
       collection.set(document.uri, this.diagnostics);
@@ -212,6 +92,48 @@ export class RobotsTxtDiagnosticUpdater {
       collection.set(document.uri, []);
     } finally {
       this.log.trace("Finished updating diagnostics for document");
+      this.diagnostics = [];
+    }
+  }
+
+  /**
+   * Checks the validity of the file itself, adding diagnostics for any issues found.
+   * @param document The text document representing the robots.txt file to be checked.
+   */
+  private async checkFile(document: vscode.TextDocument): Promise<void> {
+    if (document.isUntitled) {
+      return;
+    }
+
+    if (path.basename(document.fileName) !== "robots.txt") {
+      // The file name is not 'robots.txt'
+      this.addDiagnostic(
+        DIAGNOSTIC_LOOKUP.FILENAME_INVALID,
+        new vscode.Range(0, 0, 0, 0),
+      );
+    }
+
+    if (document.encoding === "utf8bom") {
+      // The file contains a UTF-8 BOM
+      this.addDiagnostic(
+        DIAGNOSTIC_LOOKUP.ENCODING_UTF8_BOM,
+        new vscode.Range(0, 0, 0, 0),
+      );
+    } else if (document.encoding !== "utf8") {
+      // The file encoding is not UTF-8
+      this.addDiagnostic(
+        DIAGNOSTIC_LOOKUP.ENCODING_NOT_UTF8,
+        new vscode.Range(0, 0, 0, 0),
+      );
+    }
+
+    const fileSize = await this.getFileSize(document);
+    if (FILE_SIZE_LIMIT < fileSize) {
+      // The file exceeds the recommended size limit of 500 KiB
+      this.addDiagnostic(
+        DIAGNOSTIC_LOOKUP.FILESIZE_LARGE,
+        new vscode.Range(0, 0, 0, 0),
+      );
     }
   }
 
@@ -220,51 +142,39 @@ export class RobotsTxtDiagnosticUpdater {
    * @param astDirective The AST node representing the directive to be checked.
    */
   private checkDirective(astDirective: AstDirective): void {
-    const REGEX_DIRECTIVE_NAME = /^[a-zA-Z]([a-zA-Z0-9_-]*[a-zA-Z])?$/;
+    const { type, name, separator, params } = astDirective;
+    const directiveInfo = DIRECTIVE_INFOS[type];
 
-    const directiveType = astDirective.type;
-    const nameToken = astDirective.name;
-
-    const directiveInfo = DIRECTIVE_INFOS[directiveType];
-
-    if (!REGEX_DIRECTIVE_NAME.test(directiveType)) {
+    if (!REGEX_DIRECTIVE_NAME.test(type)) {
       // The directive name is invalid
-      this.addDiagnostic(
-        DIAGNOSTIC_CODES.DIRECTIVE_NAME_INVALID,
-        nameToken.range,
-      );
+      this.addDiagnostic(DIAGNOSTIC_LOOKUP.DIRECTIVE_NAME_INVALID, name.range);
     } else if (!directiveInfo) {
       // The directive is unknown
-      this.addDiagnostic(DIAGNOSTIC_CODES.DIRECTIVE_UNKNOWN, nameToken.range);
+      this.addDiagnostic(DIAGNOSTIC_LOOKUP.DIRECTIVE_UNKNOWN, name.range);
     }
 
-    if (astDirective.separator === undefined) {
+    if (separator === undefined) {
       // The directive is missing a colon
       this.addDiagnostic(
-        DIAGNOSTIC_CODES.DIRECTIVE_MISSING_COLON,
-        nameToken.range,
+        DIAGNOSTIC_LOOKUP.DIRECTIVE_MISSING_COLON,
+        new vscode.Range(name.range.end, name.range.end),
       );
       return;
     }
 
     if (!directiveInfo) {
+      // If the directive is unknown, we cannot validate its parameters
       return;
     }
 
     if (directiveInfo.isDeprecated) {
       // The directive is deprecated
-      this.addDiagnostic(
-        DIAGNOSTIC_CODES.DIRECTIVE_DEPRECATED,
-        nameToken.range,
-      );
+      this.addDiagnostic(DIAGNOSTIC_LOOKUP.DIRECTIVE_DEPRECATED, name.range);
     }
 
-    const maxParams = Math.min(
-      directiveInfo.params.length,
-      astDirective.params.length,
-    );
+    const maxParams = Math.min(directiveInfo.params.length, params.length);
     for (let i = 0; i < maxParams; i++) {
-      this.checkParameter(directiveInfo.params[i]!, astDirective.params[i]!);
+      this.checkParameter(directiveInfo.params[i]!, params[i]!);
     }
   }
 
@@ -305,11 +215,10 @@ export class RobotsTxtDiagnosticUpdater {
    * @param productToken The AST token representing the product token parameter to be checked.
    */
   private checkParamProductToken(productToken: Span): void {
-    const REGEX_PRODUCT_TOKEN = /^([a-zA-Z_-]+$|^\*)$/;
     if (!REGEX_PRODUCT_TOKEN.test(productToken.text)) {
       // The product token is invalid
       this.addDiagnostic(
-        DIAGNOSTIC_CODES.PRODUCT_TOKEN_INVALID,
+        DIAGNOSTIC_LOOKUP.PRODUCT_TOKEN_INVALID,
         productToken.range,
       );
     }
@@ -320,41 +229,41 @@ export class RobotsTxtDiagnosticUpdater {
    * @param paramToken The AST token representing the path pattern parameter to be checked.
    */
   private checkParamPathPattern(paramToken: Span): void {
-    if (paramToken.text === "") {
+    if (isEmptySpan(paramToken)) {
       return;
     }
-    if (!/^[-$%&*./0-9=?A-Z_a-z~]+$/.test(paramToken.text)) {
+    if (!REGEX_ENCODED_PATH.test(paramToken.text)) {
       // The path pattern contains unencoded characters that should be URL-encoded
       this.addDiagnostic(
-        DIAGNOSTIC_CODES.PATH_PATTERN_INVALID_URLENCODE,
+        DIAGNOSTIC_LOOKUP.PATH_PATTERN_INVALID_URLENCODE,
         paramToken.range,
       );
     }
     if (!paramToken.text.startsWith("/")) {
       // The path pattern does not start with a slash
       this.addDiagnostic(
-        DIAGNOSTIC_CODES.PATH_PATTERN_NOT_START_SLASH,
+        DIAGNOSTIC_LOOKUP.PATH_PATTERN_NOT_START_SLASH,
         paramToken.range,
       );
     }
     if (paramToken.text.lastIndexOf("$", paramToken.text.length - 2) !== -1) {
       // The '$' character is only allowed at the end of the path pattern
       this.addDiagnostic(
-        DIAGNOSTIC_CODES.PATH_PATTERN_DOLLAR,
+        DIAGNOSTIC_LOOKUP.PATH_PATTERN_DOLLAR,
         paramToken.range,
       );
     }
     if (paramToken.text.endsWith("*")) {
       // The wildcard is not necessary at the end of the path pattern
       this.addDiagnostic(
-        DIAGNOSTIC_CODES.PATH_PATTERN_UNNECESSARY_WILDCARD,
+        DIAGNOSTIC_LOOKUP.PATH_PATTERN_UNNECESSARY_WILDCARD,
         paramToken.range,
       );
     }
     if (paramToken.text.includes("**")) {
       // The '**' pattern is not necessary (use a single '*')
       this.addDiagnostic(
-        DIAGNOSTIC_CODES.PATH_PATTERN_DOUBLE_ASTERISK,
+        DIAGNOSTIC_LOOKUP.PATH_PATTERN_DOUBLE_ASTERISK,
         paramToken.range,
       );
     }
@@ -367,7 +276,7 @@ export class RobotsTxtDiagnosticUpdater {
   private checkParamUrl(urlToken: Span): void {
     if (!this.isValidUri(urlToken.text)) {
       // The directive value is not a valid URL
-      this.addDiagnostic(DIAGNOSTIC_CODES.URL_INVALID, urlToken.range);
+      this.addDiagnostic(DIAGNOSTIC_LOOKUP.URL_INVALID, urlToken.range);
     }
   }
 
@@ -376,14 +285,14 @@ export class RobotsTxtDiagnosticUpdater {
    * @param paramToken The AST token representing the numeric parameter to be checked.
    */
   private checkParamNumeric(paramToken: Span): void {
-    if (!/^\d+$/.test(paramToken.text)) {
+    if (!REGEX_NUMERIC.test(paramToken.text)) {
       // The directive value is not a numeric
-      this.addDiagnostic(DIAGNOSTIC_CODES.NOT_NUMERIC, paramToken.range);
+      this.addDiagnostic(DIAGNOSTIC_LOOKUP.NOT_NUMERIC, paramToken.range);
     }
-    if (/^0+\d+$/.test(paramToken.text)) {
+    if (REGEX_LEADING_ZEROS.test(paramToken.text)) {
       // The directive value has leading zeros
       this.addDiagnostic(
-        DIAGNOSTIC_CODES.NUMBER_LEADING_ZERO,
+        DIAGNOSTIC_LOOKUP.NUMBER_LEADING_ZERO,
         paramToken.range,
       );
     }
@@ -394,34 +303,15 @@ export class RobotsTxtDiagnosticUpdater {
    * @param paramToken The AST token representing the query parameters parameter to be checked.
    */
   private checkParamQueryParams(paramToken: Span): void {
-    if (!/^\w+(\&\w+)*$/.test(paramToken.text)) {
+    if (!REGEX_QUERY_PARAMS.test(paramToken.text)) {
       // The directive value is invalid (query parameters should be in the format of 'key1&key2&key3')
-      this.addDiagnostic(DIAGNOSTIC_CODES.PARAM_INVALID, paramToken.range);
+      this.addDiagnostic(DIAGNOSTIC_LOOKUP.PARAM_INVALID, paramToken.range);
     }
   }
 
-  /**
-   * Checks if the given document has a valid file name (robots.txt).
-   * @param document The text document to check.
-   * @return True if the file name is valid, false otherwise.
-   */
-  private isValidFileName(document: vscode.TextDocument): boolean {
-    return path.basename(document.fileName) === "robots.txt";
-  }
-
-  /**
-   * Checks if the given document has a valid file size (not exceeding 500 KiB).
-   * @param document The text document to check.
-   * @returns True if the file size is valid, false otherwise.
-   */
-  private async isValidFileSize(
-    document: vscode.TextDocument,
-  ): Promise<boolean> {
-    if (document.isUntitled) {
-      return true;
-    }
+  private async getFileSize(document: vscode.TextDocument): Promise<number> {
     const stat = await vscode.workspace.fs.stat(document.uri);
-    return stat.size <= 500 * 1024;
+    return stat.size;
   }
 
   /**
